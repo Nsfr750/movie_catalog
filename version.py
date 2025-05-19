7cf2bd592c84a860230e0d8732f9ff5e7a452a05
Nsfr750 <nsfr750@gmail.com>
2025-05-19 19:02:09 +0200

    Update version.py

--- version.py
+++ version.py
@@ -7,11 +7,11 @@ for the project.
 
 # Version information follows Semantic Versioning 2.0.0 (https://semver.org/)
 VERSION_MAJOR = 1
-VERSION_MINOR = 5
+VERSION_MINOR = 6
 VERSION_PATCH = 0
 
 # Additional version qualifiers
-VERSION_QUALIFIER = 'beta'  # Could be 'alpha', 'beta', 'rc', or ''
+VERSION_QUALIFIER = ''  # Could be 'alpha', 'beta', 'rc', or ''
 
 def get_version():
     """
